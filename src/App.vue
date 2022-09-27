89e90a1ba559ab268aab4a562239c3268a33c98a
Jiale <2769822284@qq.com>
2022-09-28 01:40:30 +0800

    master： 测试git记录改动

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,8 @@ export default {
       this.$store.replaceState(Object.assign({}, this.$store.state,JSON.parse(sessionStorage.getItem("store"))))
     }
 
+
+
     //在页面刷新时将vuex里的信息保存到sessionStorage里
     window.addEventListener("beforeunload",()=>{
       sessionStorage.setItem("store",JSON.stringify(this.$store.state))
